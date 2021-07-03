0a5d08fa64e662229bc36a38dffeb04c7ddbf3fd
Gui-dev <adrianon10@gmail.com>
2021-07-03 02:54:13 -0300

    feat: add context AuthContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 import './services/firebase'
+import { AuthContextProvider } from './hooks/AuthContext'
 import { Routes } from './routes'
 import { GlobalStyle } from './assets/style/global'
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <GlobalStyle />
       <Routes />
-    </>
+    </AuthContextProvider>
   )
 }
 
