ed8fe7db67b0bebbbcd262d6307d0ea9d4c06e93
Gui-dev <adrianon10@gmail.com>
2021-07-06 02:22:54 -0300

    feat: add component RoomCode and get param id from url

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import logoImg from './../../assets/images/logo.svg'
 import { Button } from '../../components/Button'
+import { RoomCode } from '../../components/RoomCode'
 import { Container, Header, Main } from './style'
 
+interface IRoomParams {
+  id: string
+}
+
 export const Room: React.FC = () => {
+  const params = useParams<IRoomParams>()
+
   return (
     <Container>
       <Header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" title="Letmeask" />
-          <div>#3287879</div>
+          <RoomCode code={ params.id }/>
         </div>
       </Header>
 
